b01eaecbf2d44b5dd4c4e87839850e2939dcbd1c
bilgerdem7 <bilgerdem7@gmail.com>
2026-04-16 01:46:50 +0300

    new commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+  ],
 })
 export class AppModule {}
